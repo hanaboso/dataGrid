60eb64aa61f536ca27829b6121f05803155ce32f
Tomas Polz <polz.t@hanaboso.com>
2020-09-01 13:38:19 +0200

    fixed new ObjectRepository namespace in Doctrine/Persistence v2

--- src/GridFilterAbstract.php
+++ src/GridFilterAbstract.php
@@ -2,7 +2,7 @@
 
 namespace Hanaboso\DataGrid;
 
-use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\Persistence\ObjectRepository;
 use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
